4a43d331d2f0288e547a04b4dccd714247a800ba
Rodrigo Santos <rorvts@gmail.com>
2021-04-06 14:33:13 +0200

    Load balancer configuration

--- linux-scalable-set.tf
+++ linux-scalable-set.tf
@@ -34,3 +34,40 @@ resource "azurerm_public_ip" "vmss" {
     domain_name_label   = random_string.fqdn.result
     tags                = var.tags
 }
+
+resource "azurerm_lb" "vmss" {
+    name                = "vmss-lb"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.vmss.name
+
+    frontend_ip_configuration {
+        name                 = "PublicIPAddress"
+        public_ip_address_id = azurerm_public_ip.vmss.id
+    }
+
+    tags = var.tags
+}
+
+resource "azurerm_lb_backend_address_pool" "vmss" {
+    loadbalancer_id     = azurerm_lb.vmss.id
+    name                = "BackendAddressPool"
+}
+
+resource "azurerm_lb_probe" "vmss" {
+    resource_group_name = azurerm_resource_group.vmss.name
+    loadbalancer_id     = azurerm_lb.vmss.id
+    name                = "ssh-running-probe"
+    port                = var.application_port
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+    resource_group_name            = azurerm_resource_group.vmss.name
+    loadbalancer_id                = azurerm_lb.vmss.id
+    name                           = "http"
+    protocol                       = "Tcp"
+    frontend_port                  = var.application_port
+    backend_port                   = var.application_port
+    backend_address_pool_id        = azurerm_lb_backend_address_pool.vmss.id
+    frontend_ip_configuration_name = "PublicIPAddress"
+    probe_id                       = azurerm_lb_probe.vmss.id
+}
