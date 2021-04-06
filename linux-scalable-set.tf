0b1b65fbcbd75adb511f5edbdf032cee2b13fb75
Rodrigo Santos <rorvts@gmail.com>
2021-04-06 16:46:50 +0200

    Terraform azure linux scale set configuration

--- linux-scalable-set.tf
+++ linux-scalable-set.tf
@@ -71,3 +71,62 @@ resource "azurerm_lb_rule" "lbnatrule" {
     frontend_ip_configuration_name = "PublicIPAddress"
     probe_id                       = azurerm_lb_probe.vmss.id
 }
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+    name                = "vmscaleset"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.vmss.name
+    upgrade_policy_mode = "Manual"
+
+    sku {
+        name     = "Standard_DS1_v2"
+        tier     = "Standard"
+        capacity = 3
+    }
+
+    storage_profile_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04-LTS"
+        version   = "latest"
+    }
+
+    storage_profile_os_disk {
+        name              = ""
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    storage_profile_data_disk {
+        lun           = 0
+        caching       = "ReadWrite"
+        create_option = "Empty"
+        disk_size_gb  = 10
+    }
+
+    os_profile {
+        computer_name_prefix = "vmlab"
+        admin_username       = var.vmss_user
+        admin_password       = var.vmss_password
+        custom_data          = file("web.conf")
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+    network_profile {
+        name    = "TerraformNetworkProfile"
+        primary = true
+
+        ip_configuration {
+            name                                  = "IPConfiguration"
+            subnet_id                             = azurerm_subnet.vmss.id
+            load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.vmss.id]
+            primary = true
+        }
+    }
+
+    tags = var.tags
+}
